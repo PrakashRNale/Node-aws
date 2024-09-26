91c96c07ef442fc01b15ba7f5a60bdc02a669b4a
PrakashRN <prakashcsedyp@gmail.com>
2024-09-26 16:01:06 +0530

    added new route

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ const app = express()
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
+
 app.get("/", (req,res,next) =>{
     return res.json("hello how are you");
 })
+
 app.get('/users', (req, res) =>{
     return res.json(users);
+})
+
+app.get('/jyoti',(req, res) =>{
+    return res.json("Jyoti is item girl")
 })
 
 app.listen(PORT, () =>{
