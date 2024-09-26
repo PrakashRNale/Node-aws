913cd95a8292e2796a15577c5f06f601dab8813e
PrakashRN <prakashcsedyp@gmail.com>
2024-09-26 15:48:37 +0530

    chagned port

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express()
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
-
+app.get("/", (req,res,next) =>{
+    return res.json("hello how are you");
+})
 app.get('/users', (req, res) =>{
     return res.json(users);
 })
