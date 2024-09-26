7c71827ba8c613b9d560fb8be57ea05020b06478
PrakashRN <prakashcsedyp@gmail.com>
2024-09-26 14:29:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const users = require("./MOCK_DATA.json");
+const app = express()
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/users', (req, res) =>{
+    return res.json(users);
+})
+
+app.listen(PORT, () =>{
+    console.log('server started at port '+PORT)
+})
