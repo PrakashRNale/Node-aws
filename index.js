1e7170fd21634ff0637465d2982d5bb2faa53c0e
PrakashRN <prakashcsedyp@gmail.com>
2024-09-26 16:35:01 +0530

    Resolved cors error

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get("/", (req,res,next) =>{
     return res.json("hello how are you");
 })
